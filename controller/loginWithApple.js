c54a95edb39e29cc2932b315d148c79a0986dc9c
Abdullah-Ikram985 <abdullahikram985933@gmail.com>
2026-01-05 16:48:36 +0500

    commit

--- controller/loginWithApple.js
+++ controller/loginWithApple.js
@@ -1,14 +1,14 @@
 // controllers/apple.controller.js
-import jwt from "jsonwebtoken";
+import jwt from 'jsonwebtoken';
 // import User from "../models/User.js";
-import { verifyAppleIdentityToken } from "../utils/appleAuth.js";
+import { verifyAppleIdentityToken } from '../utils/appleAuth.js';
 
 export const appleLogin = async (req, res) => {
   try {
     const { identityToken } = req.body;
 
     if (!identityToken) {
-      return res.status(400).json({ message: "identityToken is required" });
+      return res.status(400).json({ message: 'identityToken is required' });
     }
 
     // 1️⃣ Verify Apple token
@@ -21,8 +21,8 @@ export const appleLogin = async (req, res) => {
     */
 
     const appleId = appleData.sub;
-    console.log("appleId", appleId);
-    console.log("appleData", appleData);
+    console.log('appleId', appleId);
+    console.log('appleData', appleData);
 
     // 2️⃣ Find user in DB
     // let existingUser = await User.findOne({ appleId });
@@ -48,10 +48,11 @@ export const appleLogin = async (req, res) => {
       success: true,
     });
   } catch (error) {
-    console.error("Apple login error:", error);
+    console.error('Apple login error:', error);
+
     return res.status(401).json({
       success: false,
-      message: "Apple authentication failed",
+      message: 'Apple authentication failed',
     });
   }
 };
