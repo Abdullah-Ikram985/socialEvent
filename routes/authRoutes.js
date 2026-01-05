b3c600bf068b45e52dfe074011a1a37af9eb0f66
Abdullah-Ikram985 <abdullahikram985933@gmail.com>
2026-01-05 16:48:36 +0500

    commit

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { loginWithGoogle } = require('../controller/loginWithGoogle');
-
+const { appleLogin } = require('../controller/loginWithApple');
 const router = express.Router();
 
-
 router.post('/google', loginWithGoogle);
+
+router.post('/apple-login', appleLogin);
 
 module.exports = router;
